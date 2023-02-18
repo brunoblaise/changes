7f4670d82fe11139112ee872b53f1581dc6d12f3
brunoblaise <brunoblaise94@gmail.com>
2023-02-18 19:43:53 +0200

    hello

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import os
 from random import randint
 
 for i in range(1, 365):
-    for j in range(0, 10):
-     d = str(i) + 'days ago'
-     with open('fle.txt', 'a') as File:
-            File.write(d +  str(randint(0, 100)) )
+    for j in range(0, randint(1, 10)):
+     d = str(i) + ' days ago'
+     with open('file.txt', 'a') as File:
+            File.write(d)
     os.system('git add .')
-    os.system('git commit -m "commit"')
+    os.system('git commit -m "commit" ')
 
 os.system('git push -u origin main')
